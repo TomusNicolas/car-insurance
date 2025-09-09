6fd19e24a0d1d410c9efb5377795dcdea638605e
Tomus Nicolas <nikolas.tomus@gmail.com>
2025-09-09 19:14:38 +0300

    restart insurancepolicy.id with 4

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -7,3 +7,5 @@ INSERT INTO car (id, vin, make, model, year_of_manufacture, owner_id) VALUES (2,
 INSERT INTO insurancepolicy (id, car_id, provider, start_date, end_date) VALUES (1, 1, 'Allianz', DATE '2024-01-01', DATE '2024-12-31');
 INSERT INTO insurancepolicy (id, car_id, provider, start_date, end_date) VALUES (2, 1, 'Groupama', DATE '2025-01-01', DATE '2026-01-01');
 INSERT INTO insurancepolicy (id, car_id, provider, start_date, end_date) VALUES (3, 2, 'Allianz', DATE '2025-03-01', DATE '2025-09-30');
+
+ALTER TABLE insurancepolicy ALTER COLUMN id RESTART WITH 4;
